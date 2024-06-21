7b6fdd117733af928ff96249e8f7fbd101dbb442
Rudreshwar-Singh <321rudreshwarsingh@gmail.com>
2024-06-21 17:47:34 +0530

    log_in_page

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,268 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+class Login_Page extends StatefulWidget {
+  const Login_Page({super.key});
+
+  @override
+  State<Login_Page> createState() => _Login_PageState();
+}
+
+class _Login_PageState extends State<Login_Page> {
+   bool _isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Flexible(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset(
+                'images/moofli_logo.jpg', // Path to your logo
+                width: 100, // Adjust width as needed
+                height: 100, // Adjust height as needed
+              ),
+            ),
+
+             
+              const SizedBox(height: 20.0),
+              const Text('Welcome!', textAlign: TextAlign.center),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                decoration:
+                    const InputDecoration(labelText: 'Email or phone number',  
+                     border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                   
+                  ),
+                  ),  
+                ),
+                const SizedBox(height: 20.0),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'Password' ,  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)), 
+                    
+                  ),),
+              ),
+        
+            const SizedBox(height: 20.0),
+        
+              Row(
+                 mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(iconAlignment: IconAlignment.end,
+                  onPressed: () {
+                    // Add your password recovery logic here
+                  },
+                  style: TextButton.styleFrom(
+                      foregroundColor: const Color.fromRGBO(155, 0, 210, 1.0)
+                      // This sets the text color
+                      ),
+                  child: const Text('Forgot your password?' , textAlign: TextAlign.right,),
+                ),
+              
+            ],
+            ),
+              ElevatedButton(
+                onPressed: () async {
+                  // Add your password recovery logic here
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(
+                      const Color.fromRGBO(155, 0, 210, 1.0)),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text('Log in ' , style: TextStyle(color: Color.fromRGBO(255,255,255,1)),),
+                ),
+              ),
+              const SizedBox(height: 10.0),
+               const Text('OR' , textAlign: TextAlign.center , style: TextStyle(fontSize: 20, ),),
+               const SizedBox(height: 10.0),
+
+
+          const Text('Sign in with ', style: TextStyle(color: Color.fromRGBO(155, 0, 210, 1.0) , fontWeight: FontWeight.bold),  textAlign: TextAlign.center),
+              
+          const SizedBox(height: 10.0),
+               Center(
+               child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              width: 317,
+              height: 70,
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+               
+                color: const Color.fromRGBO(241, 244, 255, 1),
+                borderRadius: BorderRadius.circular(30.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 10.0,
+                    spreadRadius: 2.0,
+                  ),
+                ],
+              ),
+              
+                  child: Column(
+                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            child: IconButton(
+                              icon: SizedBox(
+                                width: 37.0,
+                                height: 37.0,
+                                child: Image.asset('images/google_logo.png'),
+                              ),
+                              onPressed: () {
+                                // Implement Google login functionality
+                              },
+                            ),
+                          ),
+                          const VerticalDivider( width: 1.0,
+                  thickness: 1.0,
+                  color: Colors.black,), // Divider between buttons
+                          Expanded(
+                            child: IconButton(
+                              icon: SizedBox(
+                                width: 34.0,
+                                height: 33.0,
+                                child: Image.asset('images/facebook_logo.png'),
+                              ),
+                              onPressed: () {
+                                // Implement Facebook login functionality
+                              },
+                            ),
+                          ),
+                          const VerticalDivider(
+                             width: 1.0,
+                  thickness: 1.0,
+                  color: Colors.black,
+                          ), // Divider between buttons
+                          Expanded(
+                            child: IconButton(
+                              icon: SizedBox(
+                                width: 37.0,
+                                height: 37.0,
+                                child: Image.asset('images/skillop_logo.png'),
+                              ),
+                              onPressed: () {
+                                // Implement Skillop login functionality
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              
+            ),
+               ),
+               const SizedBox(height: 10.0),
+
+           RichText( textAlign: TextAlign.center,
+              text: const TextSpan(
+                text: '''By signing up or logging in, you agree to 
+  our ''', 
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16.0,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Terms of service',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      textBaseline: TextBaseline.alphabetic
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' and ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'Privacy policy',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+             
+             
+             
+        
+              const SizedBox(height: 20.0),
+              const Text('Don\'t have an account?', textAlign: TextAlign.center),
+              TextButton(
+                onPressed: () {
+                  // Add your password recovery logic here
+                },
+                style: TextButton.styleFrom(
+                    foregroundColor: const Color.fromRGBO(155, 0, 210, 1.0)
+                    // This sets the text color
+                    ),
+                child: const Text('Sign Up'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+ // ElevatedButton.icon(
+    //   icon: Image.asset('assets/google_logo.png'), // Replace with your asset path
+    //   label: const Text('Sign in with Google'),
+    //   onPressed: () {
+    //     // Handle Google sign-in
+    //   },
+    // ),
+
+     // ElevatedButton.icon(
+    //   icon: Image.asset('images/facebook_logo.png'), // Replace with your asset path
+    //   label: const Text('Sign in with Facebook'),
+    //   onPressed: () {
+    //     // Handle Facebook sign-in
+    //   },
+    // ),
+    // ElevatedButton.icon(      
+    //   icon: Image.asset('images/google_logo.png'), // Replace with your asset path
+    //   label: const Text('Sign in with Apple'),
+    //   onPressed: () {
+    //     // Handle Apple sign-in
+    //   },
+    // ),
+
+     // SignInButton(
+              //   Buttons.Google,
+              //   text: "Sign in with Google",
+              //   onPressed: () {
+              //     // Add your Google sign-in logic here
+              //   },
+              // ),
+              // SignInButton(
+              //   Buttons.Facebook,
+              //   text: "Sign in with Facebook",
+              //   onPressed: () {
+              //     // Add your Facebook sign-in logic here
+              //   },
+              // ),
+              // const SizedBox(height: 20.0),
+              
+              
