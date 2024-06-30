553452176d9cd533fa3c8cc048538746c39a9d72
Atiyab <atiyab.ahmad1947@gmail.com>
2024-06-30 10:20:28 +0530

    resolving verified page error

--- lib/screens/verified_page.dart
+++ lib/screens/verified_page.dart
@@ -1,51 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: VerifiedScreen(),
-        ),
-      ),
-    );
-  }
-}
-
 class VerifiedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Your email id and phone number is now successfully verified.',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 20.0,
-          ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Your email id and phone number is now successfully verified.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              width: 184.0,
+              height: 176.0,
+              child: Icon(
+                Icons.check_circle_rounded,
+                size: 150.0,
+                color: Colors.purple,
+              ),
+              // decoration: BoxDecoration(
+              //   color: Colors.purple.withOpacity(0.1),
+              //   shape: BoxShape.circle,
+              // ),
+            ),
+          ],
         ),
-        SizedBox(height: 20.0),
-        Container(
-          width: 184.0,
-          height: 176.0,
-          child: Icon(
-            Icons.check_circle_rounded,
-            size: 150.0,
-            color: Colors.purple,
-          ),
-          // decoration: BoxDecoration(
-          //   color: Colors.purple.withOpacity(0.1),
-          //   shape: BoxShape.circle,
-          // ),
-        ),
-      ],
+      ),
     );
   }
 }
